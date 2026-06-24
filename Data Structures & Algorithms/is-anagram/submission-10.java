3b842b4ce616c7e69fe2c98cd5d9f7dd00924e71
Alvaro Burgos <78761520+AlvaroJBurgos@users.noreply.github.com>
2026-06-24 17:25:30 +0200

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        System.out.println('b' - 'a');
+        if (s.length() != t.length())
+            return false;
+
+        Map<Character, Integer> mapS = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (mapS.containsKey(c)) {
+                int newCount = mapS.get(c) + 1;
+                mapS.put(c, newCount);
+            } else {
+                mapS.put(c, 1);
+            }
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!mapS.containsKey(c))
+                return false;
+            else {
+                int newCount = mapS.get(c) - 1;
+                if (newCount == 0) {
+                    mapS.remove(c);
+                } else
+                    mapS.put(c, newCount);
+            }
+        }
+        return true;
+    }
+}
